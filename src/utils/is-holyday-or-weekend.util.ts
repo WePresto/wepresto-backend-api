a5036f4c145d3d751775fb798a0963932c557798
Cristian David Ippolito Carrillo <cristiandavidippolito@gmail.com>
2023-08-24 14:22:00 +0000

    feat: is holyday or weekend logging

--- src/utils/is-holyday-or-weekend.util.ts
+++ src/utils/is-holyday-or-weekend.util.ts
@@ -26,8 +26,16 @@ export const isHolidayOrWeekend = async (
     day,
   });
   */
+  // eslint-disable-next-line no-console
+  console.log('isHolidayOrWeekend');
+  // eslint-disable-next-line no-console
+  console.log('date:', date);
 
   const dayOfWeek = date.getDay();
+  // eslint-disable-next-line no-console
+  console.log('dayOfWeek:', dayOfWeek);
+  // eslint-disable-next-line no-console
+  console.log('---');
 
   // eslint-disable-next-line prettier/prettier
   // const dateToCompare = `${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`;
