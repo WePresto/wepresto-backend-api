431a57910f9fe6c8bd0a435841f8e133fa549996
Cristian David Ippolito Carrillo <cristiandavidippolito@gmail.com>
2023-10-01 00:43:18 +0000

    feat: better loan amount validation

--- src/utils/validate-amount-by-country.ts
+++ src/utils/validate-amount-by-country.ts
@@ -1,5 +1,9 @@
 const countries = {
-  CO: 10000,
+  CO: {
+    divisor: 10000,
+    min: 250000,
+    max: 1500000,
+  },
 };
 
 export const validateAmountByCountry = (
@@ -8,10 +12,22 @@ export const validateAmountByCountry = (
 ) => {
   const parsedAmount = typeof amount === 'string' ? parseInt(amount) : amount;
 
-  const r = parsedAmount % countries[countryCode];
+  const r = parsedAmount % countries[countryCode].divisor;
 
   if (r !== 0) {
     throw new Error(`amount must be a multiple of ${countries[countryCode]}`);
+  }
+
+  if (parsedAmount < countries[countryCode].min) {
+    throw new Error(
+      `amount must be greater than or equal to ${countries[countryCode].min}`,
+    );
+  }
+
+  if (parsedAmount > countries[countryCode].max) {
+    throw new Error(
+      `amount must be less than or equal to ${countries[countryCode].max}`,
+    );
   }
 
   return true;
